61f23fdea6ea69c5978583c55139a3234b0133be
Ismail Bentabet <ismailbentabett@gmail.com>
2024-12-07 15:30:50 +0100

    Add LoadingScreen component to display loading state

---
+++ screens/LoadingScreen.tsx
@@ -0,0 +1,13 @@
+//loadng screen
+
+import React from "react";
+
+import { View, Text } from "react-native";
+
+export default function LoadingScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Loading...</Text>
+    </View>
+  );
+}
