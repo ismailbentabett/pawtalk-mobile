10cb6808a184ffc6751721722ba02c1e529c35eb
Ismail Bentabet <ismailbentabett@gmail.com>
2024-12-07 15:31:07 +0100

    Add navigation types for Root, Auth, and App stacks

---
+++ types/navigation.ts
@@ -0,0 +1,15 @@
+export type RootStackParamList = {
+    Auth: undefined;
+    App: undefined;
+  };
+  
+  export type AuthStackParamList = {
+    Login: undefined;
+    Register: undefined;
+  };
+  
+  export type AppStackParamList = {
+    Home: undefined;
+    Profile: undefined;
+    Settings: undefined;
+  };
